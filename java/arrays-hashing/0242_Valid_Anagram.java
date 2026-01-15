eb27d0655aafa203b9e63ee6dc972058becba184
Sayli Narkhede <saylinarkhede@yahoo.com>
2026-01-15 22:15:10 +0530

    loop syntax correction, add missing index i in charAt and variable correction

--- java/arrays-hashing/0242_Valid_Anagram.java
+++ java/arrays-hashing/0242_Valid_Anagram.java
@@ -7,16 +7,17 @@ class Solution {
         Map<Character, Integer> countS = new HashMap<>();
         Map<Character, Integer> countT = new HashMap<>();
 
-        for (int i, i < s.length(), i++) {
-            char c1 = s.charAt();
-            char c2 = t.chatAt();
+        for (int i = 0, i < s.length(), i++) {
+            char c1 = s.charAt(i);
+            char c2 = t.chatAt(i);
 
             countS.put(c1, countS.getOrDefault(c1, 0) + 1);
-            countT.put(c1, countT.getOrDefault(c2, 0) + 1);
+            countT.put(c2, countT.getOrDefault(c2, 0) + 1);
         }
         return countS.equals(countT);
     }
 }
+// O(n), O(1)
 
 public class Main {
     public static void main(String[] args) {
@@ -26,3 +27,4 @@ public class Main {
         System.out.println(sol.isAnagram(s,t));
     }
 }
+
