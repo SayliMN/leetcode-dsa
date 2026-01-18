d87a20efd37bfb94af46c944ec29414a9da818dd
Sayli Narkhede <saylinarkhede@yahoo.com>
2026-01-18 13:14:18 +0530

    update increment logic

--- java/arrays-hashing/0122_Best_Time_to_Buy_and_Sell_Stock_II.java
+++ java/arrays-hashing/0122_Best_Time_to_Buy_and_Sell_Stock_II.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int[] maxProfit(int[] prices) {
+    public int maxProfit(int[] prices) {
         int l = 0;
         int r = 0;
         int profit = 0;
@@ -7,7 +7,7 @@ class Solution {
             if (prices[r] > prices[l]) {
                 profit += prices[r] - prices[l];
             }
-            l++;
+            l = r;
             r++;
         }
         return profit;
@@ -15,7 +15,7 @@ class Solution {
 }
 
 class Main {
-    public static void main(Str[] args) {
+    public static void main(String[] args) {
         Solution sol = new Solution();
         int[] prices = {7,1,2,3,4,5,6};
         System.out.println(sol.maxProfit(prices));
